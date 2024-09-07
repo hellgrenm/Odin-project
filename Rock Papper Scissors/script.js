9ccf5ba4401ba93873b5ff922399778e119a84b7
hellgrenm <marthell106@student.kau.se>
2024-09-07 19:20:41 +0200

    Update script.js

--- Rock Papper Scissors/script.js
+++ Rock Papper Scissors/script.js
@@ -1,86 +1,123 @@
 console.log('Hello World');
+let statusText = document.getElementById('result');
+let playerChoice = 0;
+let computersChoice = 0;
+let humanScore = 0;
+let computerScore =0;
+let turns = 0;
+
+window.addEventListener('load', function(){
+    document.getElementById('start').addEventListener('click', hideStart);
+    document.getElementById('rock').addEventListener('click', getHuman);
+    document.getElementById('paper').addEventListener('click', getHuman);
+    document.getElementById('scissor').addEventListener('click', getHuman);
+})
 
 function getComputerChoice(){
     let random = Math.random();
 
     if (random < 1/3){
-        return "rock";
-    } else if (2/3){
-        return "paper";
-    } else return "scissors";
+        return computersChoice=1; //rock
+    } else if (random < 2/3){ //paper
+        return computersChoice=2;
+    } else return computersChoice=3; //scissor
 
 }
 
-function getHumanChoice(){
-    let choice = prompt('Rock, paper or scissors?');
+function hideStart(){ //Hides the start button and show rock paper scissor buttons.
+    let playButtons = document.querySelectorAll('button');
+    playButtons.forEach(button => {
+        button.classList.toggle('hidden');
+      });
+      statusText.textContent='Lets play!';
+}
 
-    if (choice.toLowerCase()==='rock' ){
-        return "rock"
-    } else if (choice.toLowerCase() === "paper"){
-        return "paper"
-    } else if (choice.toLowerCase() === "scissors"){
-        return "scissors";
+function getHuman(e){
+    console.log(e.target.id);
+    if (e.target.id==='rock'){
+        playerChoice=1;
+    } else if (e.target.id==='paper'){
+        playerChoice=2;
+    } else playerChoice =3;
+    computersChoice = getComputerChoice();
+    playGame();
+}
+
+function gameOver (){
+    let playButtons = document.querySelectorAll('button');
+    if (humanScore > computerScore){
+        console.log('You won!')
+        statusText.textContent = 'You won!';
+        
+    } else if (humanScore < computerScore){
+        statusText.textContent = 'You lose!';
+    } else if (humanScore === computerScore){
+        statusText.textContent='Its a tie!'
     }
-
-}
-
-
-
-
+      playButtons.forEach(button => {
+        button.classList.toggle('hidden');
+      });
+      turns=0;
+      humanScore=0;
+      computerScore=0;
+};
+    
 
 
 
 function playGame (){
-
-    let humanScore = 0;
-    let computerScore = 0;
+ 
+    playRound(playerChoice, computersChoice)
+    console.log('antal turns är ' + turns);
     function playRound (humanChoice, computerChoice ){
 
-        if (humanChoice === "rock"){
-            if (computerChoice === "rock"){
+        if (humanChoice === 1){
+            if (computerChoice === 1){
                 console.log('Tie!');
-            } else if (computerChoice === "paper"){
+                turns++;
+            } else if (computerChoice === 2){
                 console.log('You lose! Paper beats rock.');
                 computerScore++;
-            } else if (computerChoice==='scissors') {
+                turns++;
+            } else if (computerChoice===3) {
                 console.log('You won! Rock beats scissor.');
                 humanScore++;
+                turns++;
             }
-        } else if (humanChoice === "paper"){
-            if (computerChoice ==="paper"){
+        } else if (humanChoice === 2){
+            if (computerChoice ===2){
+                turns++;
                 console.log('Tie!');
-            } else if (computerChoice === "rock"){
+            } else if (computerChoice === 1){
+                turns++;
                 console.log('You won! Paper beats rock.');
                 humanScore++;
-            } else if (computerChoice ==="scissors"){
+            } else if (computerChoice ===3){
                 console.log('You lose! Scissor beats paper.');
+                turns++;
                 computerScore++;
             }
-        } else if (humanChoice === "scissors"){
-            if (computerChoice ==="scissors"){
+        } else if (humanChoice === 3){
+            if (computerChoice ===3){
+                turns++;
                 console.log('Tie!');
-            } else if ( computerChoice ==='paper'){
+            } else if ( computerChoice ===2){
+                turns++;
                 console.log('You won! Scissors beats paper.');
                 humanScore++;
-            } else if (computerChoice ==='rock'){
+            } else if (computerChoice ===1){
+                turns++;
                 console.log('You lose! Rock beats scissor');
                 computerScore++;
             }
         }
-    }
-    for (let i = 0; i<5;i++){    
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound (humanSelection, computerSelection);
-    }
+        statusText.textContent = 'Maximum turns is 5, now at : ' + turns + '. The score is: Human player ' + humanScore + '. Computer: ' + computerScore;
 
-    if (humanScore > computerScore){
-        console.log('You won!')
-    } else if (humanScore < computerScore){
-        console.log('You lose!');
     }
-}
+    
  
-playGame();
-
-
+    if (turns==5){
+        gameOver();
+    }
+ 
+}
