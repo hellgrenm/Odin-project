c8eb0fad91f2354bf979b1fb255956509f1e77ab
hellgrenm <marthell106@student.kau.se>
2024-09-08 21:11:04 +0200

    div creator finished

--- Etch-a-sketch/script.js
+++ Etch-a-sketch/script.js
@@ -1,15 +1,55 @@
+let startAmount = 13;
+let resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', reset);
+let startButton = document.getElementById('start');
+startButton.addEventListener('click', changeSize);
+
 
 window.addEventListener('load', function(){
-    createDivs();
-
+    createDivs(startAmount);
 });
 
-function createDivs (){ // Create 16 divs
+function createDivs (amount){ 
+    let divRef = document.getElementById('mainDiv');
+    for (let i = 0; i < amount; i++){
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute('class', 'column');
+        for (let j = 0; j < amount; j++){
+            let innerDiv = document.createElement('div');
+            innerDiv.setAttribute('class', 'inner');
+            newDiv.appendChild(innerDiv);
+        }
+    divRef.appendChild(newDiv);
+    } 
+
+    const divs = document.querySelectorAll('#mainDiv div');
+
+divs.forEach(div => {
+    div.addEventListener('mouseover', function() {
+        this.classList.add('changed');
+    });
+
+});
+};
+
+function reset (){
     let divRef = document.getElementById('mainDiv');
 
-    for (let i = 0; i < 16; i++){
-        let newDiv = document.createElement('div');
-        divRef.appendChild(newDiv);
+    while(divRef.hasChildNodes() ){
+        divRef.removeChild(divRef.firstChild);
     }
+    createDivs(startAmount);
+}
 
+function changeSize(){
+    let numInput = document.getElementById('size').value;
+    let divRef = document.getElementById('mainDiv');
+    if (!isNaN(numInput) && numInput <= 13 ){ // if input is a  number and less than 13, remove old divs and create new ones. Looks bad for now over 13.
+        while(divRef.hasChildNodes() ){
+            divRef.removeChild(divRef.firstChild);
+        }
+    createDivs(numInput);
+    } 
+    else console.log('Enter valid number.');
 };
+
